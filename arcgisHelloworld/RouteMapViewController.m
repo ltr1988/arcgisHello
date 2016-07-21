b9b63b0855ac51ae9b1c21ed43308f80a3f5824d
ltr1988 <ltr_1988@163.com>
2016-07-21 17:43:11 +0800

    up

--- arcgisHelloworld/RouteMapViewController.m
+++ arcgisHelloworld/RouteMapViewController.m
@@ -7,11 +7,40 @@
 //
 
 #import "RouteMapViewController.h"
+#import "MapViewManager.h"
 
+@interface RouteMapViewController()
+{
+    UIImageView * anchorView;
+}
+
+@end
 @implementation RouteMapViewController
+
+-(void) viewDidLoad
+{
+    [super viewDidLoad];
+    anchorView = [UIImageView new];
+    anchorView.bounds = CGRectMake(0, 0, 36,36);
+    anchorView.center = self.view.center;
+    anchorView.image = [UIImage imageNamed:@"RedPushpin"];
+}
+
 -(void) viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
+    self.mapView = [MapViewManager sharedMapView];
+    if (anchorView) {
+        [self.mapView addSubview:anchorView];
+    }
+}
+
+-(void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    if (anchorView) {
+        [anchorView removeFromSuperview];
+    }
+    self.mapView = nil;
 }
 @end
