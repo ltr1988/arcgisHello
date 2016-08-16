e6256c8f4480f4795d153ecd46c136f7f767b720
ltr1988 <ltr_1988@163.com>
2016-08-16 11:14:50 +0800

    up

--- functions/Util/HttpManager.m
+++ functions/Util/HttpManager.m
@@ -12,9 +12,46 @@
 #pragma mark - 创建请求者
 +(AFHTTPSessionManager *)manager
 {
+    return [HttpManager managerWithType:HttpTaskType_Foreground];
+}
+
++(AFHTTPSessionManager *)jsonManager
+{
+    return [HttpManager jsonManagerWithType:HttpTaskType_Foreground];
+}
+
++(AFHTTPSessionManager *)managerWithType:(HttpTaskType)type
+{
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     // 超时时间
-    manager.requestSerializer.timeoutInterval = kForegroundTimeout;
+    switch (type) {
+        case HttpTaskType_Foreground: {
+            
+            manager.requestSerializer.timeoutInterval = kForegroundTimeout;
+            break;
+        }
+        case HttpTaskType_Asynchronous: {
+            
+            manager.requestSerializer.timeoutInterval = kAsynchronousTimeout;
+            break;
+        }
+        case HttpTaskType_Synchronous: {
+            
+            manager.requestSerializer.timeoutInterval = kSynchronousTimeout;
+            break;
+        }
+        case HttpTaskType_FileRequest: {
+            
+            manager.requestSerializer.timeoutInterval = kFileRequestTimeout;
+            break;
+        }
+        default:
+        {
+            
+            manager.requestSerializer.timeoutInterval = kForegroundTimeout;
+            break;
+        }
+    }
     
     // 声明上传的是json格式的参数，需要你和后台约定好，不然会出现后台无法获取到你上传的参数问题
     manager.requestSerializer = [AFHTTPRequestSerializer serializer]; // 上传普通格式
@@ -25,11 +62,38 @@
     return manager;
 }
 
-+(AFHTTPSessionManager *)jsonManager
++(AFHTTPSessionManager *)jsonManagerWithType:(HttpTaskType)type
 {
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     // 超时时间
-    manager.requestSerializer.timeoutInterval = kForegroundTimeout;
+    switch (type) {
+        case HttpTaskType_Foreground: {
+            
+            manager.requestSerializer.timeoutInterval = kForegroundTimeout;
+            break;
+        }
+        case HttpTaskType_Asynchronous: {
+            
+            manager.requestSerializer.timeoutInterval = kAsynchronousTimeout;
+            break;
+        }
+        case HttpTaskType_Synchronous: {
+            
+            manager.requestSerializer.timeoutInterval = kSynchronousTimeout;
+            break;
+        }
+        case HttpTaskType_FileRequest: {
+            
+            manager.requestSerializer.timeoutInterval = kFileRequestTimeout;
+            break;
+        }
+        default:
+        {
+            
+            manager.requestSerializer.timeoutInterval = kForegroundTimeout;
+            break;
+        }
+    }
     
     // 声明上传的是json格式的参数，需要你和后台约定好，不然会出现后台无法获取到你上传的参数问题
     manager.requestSerializer = [AFHTTPRequestSerializer serializer]; // 上传普通格式
@@ -38,53 +102,6 @@
     // 声明获取到的数据格式
     manager.responseSerializer = [AFJSONResponseSerializer serializer];
     return manager;
-}
-
-+ (void)loginWithUserAccount:(NSString *)account password:(NSString *)password success:(SuccessBlock)success fail:(AFNErrorBlock)fail
-{
-    // 将请求参数放在请求的字典里
-    NSDictionary *param = @{@"phoneNumber":account, @"password":@"f379eaf3c831b04de153469d1bec345e"};
-    // 创建请求类
-    AFHTTPSessionManager *manager = [self manager];
-    [manager POST:@"http://pm.yunhan-china.com/index.php/api_common/login"
-       parameters:param
-         progress:^(NSProgress * _Nonnull uploadProgress) {
-             // 这里可以获取到目前数据请求的进度
-         } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-             // 请求成功
-             if(responseObject){
-                 NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
-                 success(dict,YES);
-             } else {
-                 success(@{@"msg":@"暂无数据"}, NO);
-             }
-         } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-             // 请求失败
-             fail(error);
-         }];
-    
-}
-
-+ (void)test
-{
-    // 将请求参数放在请求的字典里
-    NSDictionary *param = @{@"key":@"最强"};
-    // 创建请求类
-    AFHTTPSessionManager *manager = [self manager];
-    [manager POST:@"http://zxautocmp.reader.qq.com/autocmp"
-       parameters:param
-         progress:^(NSProgress * _Nonnull uploadProgress) {
-             // 这里可以获取到目前数据请求的进度
-         } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-             // 请求成功
-             if(responseObject){
-                 NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
-             } else {
-             }
-         } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-             // 请求失败
-         }];
-
 }
 @end
 
